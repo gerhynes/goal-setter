de193d3454bd83a19467bbb23ecdce108e52af34
Gerard Hynes <gkhynes@gmail.com>
2022-04-08 14:50:27 +0100

    Protect routes with auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,12 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    res.status(201).json({ _id: user.id, name: user.name, email: user.email });
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
   } else {
     res.status(400);
     throw new Error("Invalid user data");
@@ -45,15 +50,38 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  // Check for user
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: " User data" });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({ _id, name, email });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d"
+  });
+};
 
 module.exports = {
   registerUser,
